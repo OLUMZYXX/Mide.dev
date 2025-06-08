0f5c33d4e127a1b7265a3184486c3769c206a4e6
OLUMZYXX <akinwumiolumide5@gmail.com>
2025-06-08 15:02:29 +0100

    refactor: update component imports and styling; add resume and profile images

--- app/page.jsx
+++ app/page.jsx
@@ -1,51 +1,52 @@
-import Photo from "@/components/Photo";
-import Socials from "@/components/Socials";
-import Stats from "@/components/Stats";
-import { Button } from "@/components/ui/button";
-import { FiDownload } from "react-icons/fi";
+import Photo from '@/components/Photo'
+import Socials from '@/components/Socials'
+import Stats from '@/components/Stats'
+import { Button } from '@/components/ui/button'
+import { FiDownload } from 'react-icons/fi'
+
 const Home = () => {
   return (
-    <section className="h-full">
-      <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Developer</span>
-            <h1 className="h1">
+    <section className='h-full'>
+      <div className='container mx-auto h-full'>
+        <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
+          <div className='text-center xl:text-left order-2 xl:order-none'>
+            <span className='text-xl'>Software Developer</span>
+            <h1 className='h1'>
               Hello I'm <br />
-              <span className="text-accent">Mide Akinwumi</span>
+              <span className='text-accent'>Mide Akinwumi</span>
             </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">
+            <p className='max-w-[500px] mb-9 text-white/80'>
               I excel at crafting beautiful and elegant digital experiences for
               brands and companies. I'm proficient in various programming
-              languages and technologies.{" "}
+              languages and technologies.{' '}
             </p>
             {/* btn and socials */}
-            <div className="flex flex-col xl:flex-row items-center gap-8">
+            <div className='flex flex-col xl:flex-row items-center gap-8'>
               <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
+                variant='outline'
+                size='lg'
+                className='uppercase flex items-center gap-2'
               >
-                <span>Download Cv</span>
-                <FiDownload className="text-xl" />
+                <a href='/assets/resume/resume.docx'>Download Cv</a>
+                <FiDownload className='text-xl' />
               </Button>
-              <div className="mb-8 xl:mb-0">
+              <div className='mb-8 xl:mb-0'>
                 <Socials
-                  containerStyle="flex gap-6 "
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-al duration-500"
+                  containerStyle='flex gap-6 '
+                  iconStyles='w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-al duration-500'
                 />
               </div>
             </div>
           </div>
           {/* Photo section */}
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+          <div className='order-1 xl:order-none mb-8 xl:mb-0'>
             <Photo />
           </div>
         </div>
       </div>
       <Stats />
     </section>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,12 +1,18 @@
-import Link from "next/link";
-import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
+import Link from 'next/link'
+import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedin />, path: "" },
-  { icon: <FaTwitter />, path: "" },
-  { icon: <FaInstagram />, path: "" },
-];
+  { icon: <FaGithub />, path: 'https://github.com/OLUMZYXX' },
+  {
+    icon: <FaLinkedin />,
+    path: 'https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile',
+  },
+  {
+    icon: <FaTwitter />,
+    path: 'https://x.com/olumzyrb?t=r0zb0ANUQtvCM7M0z3RVbw&s=08',
+  },
+  { icon: <FaInstagram />, path: 'https://instagram.com/olumzyxx' },
+]
 
 const Socials = ({ containerStyle, iconStyles }) => {
   return (
@@ -16,10 +22,10 @@ const Socials = ({ containerStyle, iconStyles }) => {
           <Link key={index} href={item.path} className={iconStyles}>
             {item.icon}
           </Link>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-export default Socials;
+export default Socials
